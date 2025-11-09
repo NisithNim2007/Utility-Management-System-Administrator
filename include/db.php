bf2f8fc17603aa45b6d3c52456e9c7d841e6d2f4
Nisith Nimtharu Welivitiya <155635480+NisithNim2007@users.noreply.github.com>
2025-11-09 13:16:08 +0530

    up

--- include/db.php
+++ include/db.php
@@ -1,9 +1,9 @@
 <?php
-$host = "localhost";
+$host = "159.65.158.217";
 $port = "1433";
-$dbname = "";
-$username = "";
-$password = "";
+$dbname = "UMS1";
+$username = "imrgroup_project_login";
+$password = "TUIY43afwejin123JKH";
 
 try {
     $dsn = "sqlsrv:Server=$host,$port;Database=$dbname";
