906b797ac17533c79a23efc7022fc2d1272f5be2
Vishaap <vppeiris@students.nsbm.ac.lk>
2025-11-27 11:00:08 +0530

    Commit 2- Moved sidebar and header files to admin repo includes folder

---
+++ include/header.php
@@ -0,0 +1,17 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <title>Utility Management System</title>
+
+  <!-- Tailwind CDN goes here -->
+  <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body class="bg-[#f0f0f0] font-sans">
+
+<!--div class="flex"-->
+
