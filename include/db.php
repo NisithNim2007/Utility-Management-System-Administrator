deef863f561b8e44849eeefc5c0367b817452404
Vishaap <vppeiris@students.nsbm.ac.lk>
2025-11-29 14:37:09 +0530

    Commit- updated db.php
    Updated the db.php inside 'include' with Ishika's funcitons

--- include/db.php
+++ include/db.php
@@ -17,4 +17,50 @@ try {
     error_log("Database connection failed." . $e->getMessage(), 0);
     die("Database connection failed. Please try again later.");
 }
+
+
+function executeQuery(PDO $conn, string $sql, array $params=[], bool $single=false
+){
+    try{
+        $stmt = $conn-> prepare($sql);
+    if(!empty($params)){
+        foreach ($params as $key => $value){
+
+            if(is_string($key)){
+                $stmt -> bindValue($key, $value);
+            }else{
+                $stmt -> bindValue($key + 1, $value);
+            }
+        }
+
+    }
+        $stmt -> execute();
+        
+        return $single
+            ? $stmt-> fetch(PDO::FETCH_ASSOC)
+            : $stmt->fetchAll(PDO::FETCH_ASSOC);
+       
+    }catch(PDOException $e){
+        die("Database error: " . $e->getMessage());
+    }
+}
+
+
+
+
+
+function executeNonQuery(PDO $conn, $sql, $params=[]){
+    try{
+        $stmt = $conn->prepare($sql);
+
+        foreach($params as $key=>$value){
+            $stmt->bindValue($key,$value);
+        }
+        $stmt->execute();
+        return "success";
+    }catch (PDOException $e){
+        return "Databse error: " . $e->getMessage();
+    }
+}
+
 ?>
